d67fc7e288d6a3fca9846209f0b1763507d0248e
Anton Webb <anton25360@gmail.com>
2019-11-29 15:16:45 +0000

    Added routes file

---
+++ Config/routes.js
@@ -0,0 +1,29 @@
+const TodosController = require('../Controllers/ToDosController');
+
+//pass the whole application into this function
+const routes = (app) => {
+
+    //get all accounts
+    app.get('/', AccountController.getAllAccounts)
+
+    //get accounts bigger than a certain balance
+    app.get('/biggerThan', AccountController.getAccountsBiggerThan)
+
+    //get accounts smaller than a certain balance
+    app.get('/smallerThan', AccountController.getAccountsSmallerThan)
+
+    //add money to an account
+    app.put('/addMoney', AccountController.addMoney)
+
+    //remove money from an account
+    app.put('/removeMoney', AccountController.addMoney)
+
+    //create new account
+    app.post('/', AccountController.createAccount)
+
+    //delete an account
+    app.delete('/:name', AccountController.deleteAccount)
+}
+
+//making it available to be used in app.js
+module.exports = routes;
